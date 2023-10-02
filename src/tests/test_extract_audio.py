5a22b18301e9ef224690fdb21d97833734909c73
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-10-02 12:56:40 -0300

    feat: Adaptation to pylint rules

--- src/tests/test_extract_audio.py
+++ src/tests/test_extract_audio.py
@@ -1,23 +1,32 @@
+"""
+Test module for the functionality in src.modules.extract_audio.
+"""
 import sys
 import os
+import unittest
+from unittest.mock import patch, Mock
+
+from src.modules import extract_audio
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
-import unittest
-from unittest.mock import patch
-from src.modules import extract_audio
-
 
 class TestExtractAudio(unittest.TestCase):
-
+    """
+    Test case class for extract_audio functionalities.
+    """
     @patch('src.modules.extract_audio.subprocess.run')
-    def test_extract_audio_from_video(self, mock_run):
-        video_path = 'input_video.mp4'
-        audio_path = 'output_audio.wav'
+    def test_extract_audio_from_video(self, mock_run: Mock) -> None:
+        """
+        Test if the extract_audio_from_video function calls subprocess.run with the correct 
+        arguments.
+        """
+        video_path: str = 'input_video.mp4'
+        audio_path: str = 'output_audio.wav'
 
         extract_audio.extract_audio_from_video(video_path, audio_path)
 
-        expected_command = [
+        expected_command: list = [
             'ffmpeg',
             '-i', video_path,
             '-ac', '1',
@@ -29,7 +38,10 @@ class TestExtractAudio(unittest.TestCase):
 
         mock_run.assert_called_once_with(expected_command, check=True)
 
-    def test_get_audio_output_name(self):
+    def test_get_audio_output_name(self) -> None:
+        """
+        Test if the get_audio_output_name function produces the correct output for given inputs.
+        """
         self.assertEqual(extract_audio.get_audio_output_name('audio'), 'audio.wav')
         self.assertEqual(extract_audio.get_audio_output_name('audio.wav'), 'audio.wav')
         self.assertEqual(extract_audio.get_audio_output_name('my.audio'), 'my.audio.wav')
