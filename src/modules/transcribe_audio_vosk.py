5c93c7bb9c718f048639bcecc780fa274ae0ebf6
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-10-02 13:43:28 -0300

    feat: Creating a time counter for transcription

--- src/modules/transcribe_audio_vosk.py
+++ src/modules/transcribe_audio_vosk.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 
 from vosk import Model, KaldiRecognizer
 
@@ -20,21 +21,37 @@ def save_to_json(data, path):
         json.dump(data, file, ensure_ascii=False, indent=4)
 
 
+def format_time(seconds: float) -> str:
+    if seconds < 60:
+        return f"{seconds:.2f} seconds"
+    elif seconds < 3600:  # 60 minutos * 60 segundos
+        return f"{seconds / 60:.2f} minutes"
+    else:
+        return f"{seconds / 3600:.2f} hours"
+    
+
 if __name__ == '__main__':
-    AUDIO_PATH = "data/processed/ptbr_example.wav"
+    AUDIO_PATH = "data/processed/ptbr_short_example.wav"
     MODEL_PATH = "models/vosk/vosk-model-small-pt-0.3"
 
     file_name = os.path.basename(AUDIO_PATH)
     json_name = os.path.splitext(file_name)[0] + ".json"
     JSON_OUTPUT_PATH = os.path.join("data/processed", json_name)
 
+    start_time = time.time()
+
     transcription = transcribe_audio_vosk(AUDIO_PATH, MODEL_PATH)
+
+    elapsed_time = time.time() - start_time
+    formatted_time = format_time(elapsed_time)
 
     data_to_save = {
         "name": file_name,
         "path": AUDIO_PATH,
+        "processing_time": formatted_time,
         "transcription": transcription
     }
 
     save_to_json(data_to_save, JSON_OUTPUT_PATH)
     print(f"Transcript saved in: {JSON_OUTPUT_PATH}")
+    print(f"Processing Time: {formatted_time}")
